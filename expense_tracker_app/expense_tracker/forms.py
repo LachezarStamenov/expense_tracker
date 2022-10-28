6228314362711cc1782ce6053394664e67e657c4
Lachezar.Stamenov <lachezar.stamenov@abv.bg>
2022-10-28 22:38:29 +0300

    delete profile and delete expense views and forms added

--- expense_tracker_app/expense_tracker/forms.py
+++ expense_tracker_app/expense_tracker/forms.py
@@ -1,3 +1,5 @@
+import os
+
 from django import forms
 
 from expense_tracker_app.expense_tracker.models import Profile, Expense
@@ -15,6 +17,19 @@ class EditProfileForm(forms.ModelForm):
         fields = ['budget', 'first_name', 'last_name', 'profile_image']
 
 
+class DeleteProfileForm(forms.ModelForm):
+    def save(self, commit=True):
+        image_path = self.instance.profile_image.path
+        self.instance.delete()
+        Expense.objects.all().delete()
+        os.remove(image_path)
+        return self.instance
+
+    class Meta:
+        model = Profile
+        fields = ()
+
+
 class CreateExpenseForm(forms.ModelForm):
     class Meta:
         model = Expense
@@ -25,3 +40,19 @@ class EditExpenseForm(forms.ModelForm):
     class Meta:
         model = Expense
         fields = '__all__'
+
+
+class DeleteExpenseForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for _, field in self.fields.items():
+            field.widget.attrs['disabled'] = 'disabled'
+            field.required = False
+
+    def save(self, commit=True):
+        self.instance.delete()
+        return self.instance
+
+    class Meta:
+        model = Expense
+        fields = ('title', 'description', 'expense_image', 'price')
--- expense_tracker_app/expense_tracker/views.py
+++ expense_tracker_app/expense_tracker/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 from expense_tracker_app.expense_tracker.forms import CreateProfileForm, CreateExpenseForm, EditExpenseForm, \
-    EditProfileForm
+    EditProfileForm, DeleteProfileForm, DeleteExpenseForm
 from expense_tracker_app.expense_tracker.models import Profile, Expense
 
 
@@ -15,7 +15,7 @@ def get_profile():
 def index(request):
     profile = get_profile()
     if not profile:
-        return redirect('create_profile')
+        return redirect('create profile')
 
     expenses = Expense.objects.all()
     budget_left = profile.budget - sum(e.price for e in expenses)
@@ -75,7 +75,21 @@ def edit_expense(request, pk):
 
 
 def delete_expense(request, pk):
-    return render(request, 'expense-delete.html')
+    expense = Expense.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = DeleteExpenseForm(request.POST, instance=expense)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = DeleteExpenseForm(instance=expense)
+
+    context = {
+        'form': form,
+        'expense': expense,
+    }
+
+    return render(request, 'expense-delete.html', context)
 
 
 def show_profile(request):
@@ -107,6 +121,20 @@ def edit_profile(request):
 
 
 def delete_profile(request):
-    return render(request, 'profile-delete.html')
+    profile = get_profile()
+    if request.method == 'POST':
+        form = DeleteProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = DeleteProfileForm(instance=profile)
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'profile-delete.html', context)
+
 
 
