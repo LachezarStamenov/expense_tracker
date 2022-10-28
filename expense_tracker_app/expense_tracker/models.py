3e80a468a4073173006a8b34df11dd762d945797
Lachezar.Stamenov <lachezar.stamenov@abv.bg>
2022-10-28 18:13:49 +0300

    profile model added

--- expense_tracker_app/expense_tracker/models.py
+++ expense_tracker_app/expense_tracker/models.py
@@ -1,3 +1,18 @@
+from django.core.validators import MinValueValidator, MinLengthValidator
 from django.db import models
 
-Profile
+from expense_tracker_app.expense_tracker.validators import MaxFileSizeInMbValidator, only_letters_validator
+
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length=15, validators=(MinLengthValidator(2), only_letters_validator))
+    last_name = models.CharField(max_length=15, validators=(MinLengthValidator(2), only_letters_validator))
+    budget = models.FloatField(default=0, validators=MinValueValidator(0))
+    profile_image = models.ImageField(
+        upload_to='profiles/',
+        null=True,
+        blank=True,
+        validators=(
+            MaxFileSizeInMbValidator(5),
+        ),
+    )
